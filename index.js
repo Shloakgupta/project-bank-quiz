f380a6c1cfdaa4037f7ab567b14ddcaaa9e8fe98
Shloakgupta <shloakgupta09@gmail.coma>
2023-12-27 12:28:11 -0600

    cleaned grammar

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 const questions = [
   {
-    question: "What is the purpose of a budget?",
+    question: "1. What is the purpose of a budget?",
     choices: ["a) To spend money without restrictions", "b) To save money for future expenses", "c) To track the number of toys owned", "d) To borrow money from friends"],
     correctAnswer: 2
   },
   {
-    question: "What does 'interest' mean in the context of finance??",
+    question: "2. What does 'interest' mean in the context of finance?",
     choices: ["a) Money earned on savings or investments", "b) Money spent on shopping", "c) Money exchanged in foreign markets", "d) Money given as gifts"],
     correctAnswer: 1
   },
   {
-    question: "How can you increase your savings?",
+    question: "3. How can you increase your savings?",
     choices: ["a) Spending more than your income", "b) Saving only during special occasion", "c) Setting aside a portion of your allowance or earnings", "d) Avoiding the concept of saving altogether"],
     correctAnswer: 3
   },
   {
-    question: " What is the role of a bank in personal finance?",
+    question: "4. What is the role of a bank in personal finance?",
     choices: ["a) To spend money on behalf of customers", "b) To provide a safe place to keep money and offer financial services", "c) To lend money with no expectation of repayment", "d) To sell goods and services to customers"],
     correctAnswer: 2
   },
   {
-    question: "What is the definition of 'credit score'?",
+    question: "5. What is the definition of 'credit score'?",
     choices: ["a) The amount of money in a savings account", "b) A numerical representation of a person's creditworthiness", "c) The price of goods and services", "d) The number of credit cards a person owns"],
     correctAnswer: 2
   },
   {
-    question: "Why is it important to have insurance?",
+    question: "6. Why is it important to have insurance?",
     choices: ["a) To make money through premiums", "b) To protect against financial losses from unexpected events", "c) To buy expensive items", "d) To show off to friends and family"],
     correctAnswer: 2
   },
   {
-    question: "What is the purpose of investing?",
+    question: "7. What is the purpose of investing?",
     choices: ["a) To spend money on luxury items", "b) To earn a return on your money over time", "c) To give money to charity", "d) To hide money from taxes"],
     correctAnswer: 2
   },
   {
-    question: "What is inflation?",
-    choices: ["a) The decrease in the prices of goods and services", "b) The increase in the value of money over time", "c) The overall rise in the prices of goods and services over time", "dd) The process of saving money in a piggy bank"],
+    question: "8. What is inflation?",
+    choices: ["a) The decrease in the prices of goods and services", "b) The increase in the value of money over time", "c) The overall rise in the prices of goods and services over time", "d) The process of saving money in a piggy bank"],
     correctAnswer: 3
   },
   {
-    question: "What is the difference between a debit card and a credit card?",
+    question: "9. What is the difference between a debit card and a credit card?",
     choices: ["a) They are the same", "b) Debit cards are linked to a bank account, while credit cards allow you to borrow money", "c) Credit cards are free, but debit cards have fees", "d) Debit cards are only for online shopping, while credit cards are for in-store purchases"],
     correctAnswer: 2
   },
   {
-    question: "What does the term 'ROI' stand for in finance?",
-    choices: ["aa) Return on Investment", "b) Rate of Inflation", "c) Running Out of Income", "d) Receiving Outstanding Income"],
+    question: "10. What does the term 'ROI' stand for in finance?",
+    choices: ["a) Return on Investment", "b) Rate of Inflation", "c) Running Out of Income", "d) Receiving Outstanding Income"],
     correctAnswer: 1
   },
 ];
